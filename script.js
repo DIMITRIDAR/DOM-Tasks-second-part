63bb5f2e433efb8e2932f4b946eaf298c045a1a9
DIMITRIDAR <daraseliadimitri@gmail.com>
2024-05-24 01:19:39 +0400

    Task 5 description

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
 // Task 4
 // In this task we have to create an HTML page with a <button> element and write a JavaScript function that,
 // when the button is clicked, displays an alert with the message "Button Clicked!"
+
+// function showAlert() {
+//   alert("Button Clicked!");
+// }
+
+// document.getElementById("buttonClick").addEventListener("click", showAlert);
+
+//Task 5
+//In this task we have to create an HTML page with a <form> element containing an input field and a submit button
+//and then write a JavaScript function that, when the form is submitted, validates the input field.If the
+//input is empty,display an error message next to the input field. If the input is not empty, display an alert with
+//the message "Form Submitted!"

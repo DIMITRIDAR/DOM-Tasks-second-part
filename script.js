5c08db7ad5b3e98d569bf48c636ae261b43c63af
DIMITRIDAR <daraseliadimitri@gmail.com>
2024-05-23 15:15:42 +0400

    Task 4 description and button element in HTML

---
+++ script.js
@@ -0,0 +1,3 @@
+// Task 4
+// In this task we have to create an HTML page with a <button> element and write a JavaScript function that,
+// when the button is clicked, displays an alert with the message "Button Clicked!"
